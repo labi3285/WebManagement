0495544cbf98af2104417c9e0e29bdd8f8b7debd
xqin23 <xin_qin@apple.com>
2023-02-02 00:00:19 +0800

    x

---
+++ assets/redirect.6d391c59.js
@@ -0,0 +1,1 @@
+import{d as n,u as s,b as u,V as c}from"./index.9909b5ce.js";const i=n({name:"Redirect",setup(){const t=s(),r=u();return c(()=>{const{params:o,query:a}=t,{path:e}=o;r.replace({path:"/"+(Array.isArray(e)?e.join("/"):e),query:a})}),()=>React.createElement("div",null)}});export{i as default};
